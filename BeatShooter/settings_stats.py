ad8876beb1b902156a24114cfdd3829138349555
rdkasimov <ol.kasimov33@gmail.com>
2025-09-16 15:16:14 +0400

    menu + 1 game

--- BeatShooter/settings_stats.py
+++ BeatShooter/settings_stats.py
@@ -1,14 +1,17 @@
 import os, sys, json
 
 def resource_path(relative_path):
-    """Возвращает корректный путь к ресурсам и при запуске .py, и при запуске .exe"""
     if hasattr(sys, '_MEIPASS'):
-        base_path = sys._MEIPASS   # папка, куда PyInstaller распаковывает временные файлы
+        base_path = sys._MEIPASS
     else:
         base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
+
+stat = {}
 
 def update_stat():
     global stat
     with open(resource_path("stats.json"), "r") as out_file:
         stat = json.load(out_file)
+
+update_stat()
